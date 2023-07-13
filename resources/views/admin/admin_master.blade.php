f152533e54caf40dd68343b0856aad2d2c645d7f
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2023-07-13 11:20:17 +0600

    updated bootstrap table

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -125,12 +125,12 @@
  @endif 
 </script>
 
-<!-- Required datatable js -->
-        <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+         <!-- Required datatable js -->
+         <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
         <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-        
-<!-- Responsive examples -->
-        <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+
+            <!-- Datatable init js -->
+        <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
 
     </body>
 
