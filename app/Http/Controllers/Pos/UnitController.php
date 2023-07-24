887d0998119064140a9ea167bc0520416bd974b7
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2023-07-24 14:17:23 +0600

    unit page done

--- app/Http/Controllers/Pos/UnitController.php
+++ app/Http/Controllers/Pos/UnitController.php
@@ -41,4 +41,43 @@ class UnitController extends Controller
         return redirect()->route('unit.all')->with($notification);
 
     }
+
+    public function UnitEdit($id){
+
+        $unit = Unit::findorFail($id);
+        return view('backend.unit.unit_edit', compact('unit'));
+
+    }// End Method
+
+    public function UnitUpdate(Request $request){
+
+        $unit_id = $request->id;
+
+        Unit::findOrFail($unit_id)->update([
+            'name' => $request->name,
+            'updated_by' => Auth::user()->id,
+            'updated_at' => Carbon::now()
+        ]);
+
+        $notification = array(
+            'message' => 'Unit Updated Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('unit.all')->with($notification);
+
+    }
+
+    public function UnitDelete($id){
+
+        Unit::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Unit Deleted Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
 }
