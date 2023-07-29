43dc1c25e27f91d2ef8e35c5930da4971c1ac11d
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2023-07-29 12:42:35 +0600

    updates

--- app/Http/Controllers/Pos/CategoryController.php
+++ app/Http/Controllers/Pos/CategoryController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
-    public function CategoryAll()
-    //
+    public function CategoryAll(){
+
+        $categories = Category::latest()->get();
+
+        return view('backend.category.category_all', compact('categories'));
+
+    }
+
+    public function CategoryAdd(){
+
+        return view('backend.category.category_add');
+
+    }
 }
---
+++ resources/views/backend/category/category_add.blade.php
@@ -0,0 +1,86 @@
+@extends('admin.admin_master')
+@section('admin')
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+<div class="page-content">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12">
+
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <h4 class="card-title">Add Category Page </h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Happy Haat</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                            <li class="breadcrumb-item active">Add Cateogry</li>
+                        </ol>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+
+                        <form method="post" action="{{ route('unit.store') }}" id="myForm">
+                            @csrf
+
+                            <div class="row mb-4">
+                                <label for="example-text-input" class="col-sm-1 col-form-label">Category Name</label>
+                                <div class="form-group col-sm-11">
+                                    <input name="name" class="form-control" type="text">
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <input type="submit" class="btn btn-info waves-effect waves-light" value="Add Unit">
+                        </form>
+
+
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+
+
+
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#myForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: 'Please Enter Category\'s Name',
+                },
+            },
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
+</script>
+
+
+
+@endsection
